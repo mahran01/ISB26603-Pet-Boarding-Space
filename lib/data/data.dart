9980f9d1749d63805a06b6cf0dc4a880a4650383
Nadziman <z6578010@gmail.com>
2023-11-25 17:33:58 +0800

    updated space_list_page

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -2,47 +2,53 @@ import 'package:pet_boarding_space/models/pet.dart';
 import 'package:pet_boarding_space/models/boarding_space.dart';
 
 class AssignedValues {
-  final List<BoardingSpace> boardingSpaces = [
+  static final List<BoardingSpace> boardingSpaces = [
     BoardingSpace(
         name: 'Cat Deluxe Cabin',
+        imageUrl: 'lib/images/CatDeluxeCabin.jpeg',
         petType: PetType.cat,
         hourlyRates: 10.00,
         dailyRates: 35.00),
     BoardingSpace(
         name: 'Cat Premium Suite',
+        imageUrl: 'lib/images/CatPremiumSuite.jpg',
         petType: PetType.cat,
         hourlyRates: 12.00,
         dailyRates: 40.00),
     BoardingSpace(
         name: 'Cat Cozy Corner',
+        imageUrl: 'lib/images/CatCozyConer.png',
         petType: PetType.cat,
         hourlyRates: 8.00,
         dailyRates: 30.00),
-    BoardingSpace(
-        name: 'Cat Spacious Room',
-        petType: PetType.cat,
-        hourlyRates: 10.00,
-        dailyRates: 35.00),
+    // BoardingSpace(
+    //     name: 'Cat Spacious Room',
+    //     petType: PetType.cat,
+    //     hourlyRates: 10.00,
+    //     dailyRates: 35.00),
     BoardingSpace(
         name: 'Dog Deluxe Cabin',
+        imageUrl: 'lib/images/DogDeluxeCabin.jpg',
         petType: PetType.dog,
         hourlyRates: 15.00,
         dailyRates: 45.00),
     BoardingSpace(
         name: 'Dog Premium Suite',
+        imageUrl: 'lib/images/DogPremiumSuite.webp',
         petType: PetType.dog,
         hourlyRates: 20.00,
         dailyRates: 55.00),
     BoardingSpace(
         name: 'Dog Cozy Corner',
+        imageUrl: 'lib/images/DogCozyConer.jpg',
         petType: PetType.dog,
         hourlyRates: 12.00,
         dailyRates: 38.00),
-    BoardingSpace(
-        name: 'Dog Spacious Room',
-        petType: PetType.dog,
-        hourlyRates: 18.00,
-        dailyRates: 50.00),
+    // BoardingSpace(
+    //     name: 'Dog Spacious Room',
+    //     petType: PetType.dog,
+    //     hourlyRates: 18.00,
+    //     dailyRates: 50.00),
   ];
 }
 
--- lib/models/boarding_space.dart
+++ lib/models/boarding_space.dart
@@ -2,11 +2,13 @@ import 'package:pet_boarding_space/models/pet.dart';
 
 class BoardingSpace {
   String name;
+  String imageUrl;
   PetType petType;
   double hourlyRates, dailyRates;
 
   BoardingSpace({
     required this.name,
+    required this.imageUrl,
     required this.petType,
     required this.hourlyRates,
     required this.dailyRates,
