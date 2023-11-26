d05436ad7180fbe64437702f57f2ac5b47cb7e55
Nadziman <z6578010@gmail.com>
2023-11-26 12:48:41 +0800

    Add bottom sheet

--- lib/pages/space_list_page.dart
+++ lib/pages/space_list_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:pet_boarding_space/models/boarding_space.dart';
 import 'package:pet_boarding_space/models/pet.dart';
@@ -12,7 +14,7 @@ class SpaceListPage extends StatefulWidget {
 }
 
 class _SpaceListPageState extends State<SpaceListPage> {
-  PetType userPet = PetType.cat; //TODO: replace with user input
+  PetType userPet = PetType.dog; //TODO: replace with user input
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,9 +32,6 @@ class _SpaceListPageState extends State<SpaceListPage> {
       ),
       body: SafeArea(
         child: Container(
-          // child: Column(
-          //   children: [
-          //     const SizedBox(height: 30),
           child: Padding(
             padding:
                 const EdgeInsets.symmetric(vertical: 8.0, horizontal: 24.0),
@@ -40,7 +39,7 @@ class _SpaceListPageState extends State<SpaceListPage> {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const SizedBox(height: 25),
+                const SizedBox(height: 35),
                 Text(
                   'Explore Your Pet Space',
                   style: Theme.of(context).textTheme.headlineLarge,
@@ -58,45 +57,191 @@ class _SpaceListPageState extends State<SpaceListPage> {
                     itemCount: AssignedValues.boardingSpaces.length,
                     itemBuilder: (BuildContext context, int index) {
                       BoardingSpace bs = AssignedValues.boardingSpaces[index];
+                      // TODO: replace it with user
                       if (userPet == bs.petType) {
                         return GestureDetector(
-                          onTap: () {},
-                          child: buildBoardingSpaceCard(
-                              AssignedValues.boardingSpaces[index]),
+                          onTap: () {
+                            showBottomSheet(
+                              context: context,
+                              builder: (BuildContext context) => Container(
+                                height: 510,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(20),
+                                    topRight: Radius.circular(20),
+                                  ),
+                                ),
+                                child: Stack(
+                                    alignment: Alignment.topCenter,
+                                    children: [
+                                      Container(
+                                        height: 300,
+                                        width: double.infinity,
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(20),
+                                            topRight: Radius.circular(20),
+                                          ),
+                                          image: DecorationImage(
+                                            image: AssetImage(bs.imageUrl),
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                      Column(
+                                        children: [
+                                          SizedBox(height: 280),
+                                          Expanded(
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                boxShadow: [
+                                                  BoxShadow(
+                                                    color: Colors.black
+                                                        .withOpacity(0.5),
+                                                    spreadRadius: 3,
+                                                    blurRadius: 5,
+                                                  )
+                                                ],
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .background,
+                                                borderRadius: BorderRadius.only(
+                                                  topLeft: Radius.circular(20),
+                                                  topRight: Radius.circular(20),
+                                                ),
+                                              ),
+                                              width: double.infinity,
+                                              child: Column(
+                                                children: [
+                                                  SizedBox(height: 5),
+                                                  Text(
+                                                    bs.name,
+                                                    style: Theme.of(context)
+                                                        .textTheme
+                                                        .headlineLarge,
+                                                  ),
+                                                  SizedBox(height: 10),
+                                                  Text(
+                                                      'Price per hour: RM ${bs.hourlyRates}'),
+                                                  SizedBox(height: 5),
+                                                  Text(
+                                                      'Price per day: RM ${bs.dailyRates}'),
+                                                  SizedBox(height: 40),
+                                                  Row(
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment
+                                                            .center,
+                                                    children: [
+                                                      ElevatedButton(
+                                                        style: ElevatedButton
+                                                            .styleFrom(
+                                                          fixedSize: const Size(
+                                                            150,
+                                                            50,
+                                                          ),
+                                                          side: BorderSide(
+                                                            color: Theme.of(
+                                                                    context)
+                                                                .primaryColor,
+                                                          ),
+                                                        ),
+                                                        child: Text('Cancel'),
+                                                        onPressed: () =>
+                                                            Navigator.pop(
+                                                                context),
+                                                      ),
+                                                      SizedBox(width: 10),
+                                                      ElevatedButton(
+                                                        style: ElevatedButton
+                                                            .styleFrom(
+                                                          fixedSize: const Size(
+                                                            150,
+                                                            50,
+                                                          ),
+                                                          primary:
+                                                              Theme.of(context)
+                                                                  .primaryColor,
+                                                        ),
+                                                        child: Text(
+                                                          'Book',
+                                                          style: TextStyle(
+                                                              color:
+                                                                  Colors.white),
+                                                        ),
+                                                        onPressed: () =>
+                                                            Navigator.pushNamed(
+                                                                context,
+                                                                "/paymentpage"),
+                                                      ),
+                                                    ],
+                                                  )
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      )
+                                    ]),
+                              ),
+                            );
+                          },
+                          child: BoardingSpaceCard(boardingSpace: bs),
                         );
+                      } else {
+                        return null;
                       }
                     },
                   ),
                 ),
-                const SizedBox(height: 100),
+                const SizedBox(height: 10),
               ],
             ),
           ),
-          //   ],
-          // // ),
         ),
       ),
     );
   }
 }
 
-Widget buildBoardingSpaceCard(BoardingSpace boardingSpaces) {
-  return Card(
-    margin: EdgeInsets.only(right: 20),
-    elevation: 0,
-    child: Column(
-      children: [
-        Container(
-          width: 300,
-          height: 310,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-            image: DecorationImage(
-                image: AssetImage(boardingSpaces.imageUrl), fit: BoxFit.cover),
-          ),
+class BoardingSpaceCard extends StatelessWidget {
+  final BoardingSpace boardingSpace;
+  const BoardingSpaceCard({super.key, required this.boardingSpace});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.only(right: 20, bottom: 40),
+      elevation: 0,
+      child: Container(
+        width: 300,
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 94, 92, 92),
+          borderRadius: BorderRadius.circular(35),
         ),
-        Text(boardingSpaces.name),
-      ],
-    ),
-  );
+        child: Column(
+          children: [
+            Container(
+              width: 300,
+              height: 310,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(35),
+                  topRight: Radius.circular(35),
+                ),
+                image: DecorationImage(
+                    image: AssetImage(boardingSpace.imageUrl),
+                    fit: BoxFit.cover),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Text(
+              boardingSpace.name,
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
