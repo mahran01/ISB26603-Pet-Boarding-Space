78c007873b8905de3a2d38d639f2c35657455bc7
Mahran Mazlan <m6mmbm.uni@gmail.com>
2023-12-01 12:20:21 +0800

    Fix card height

--- lib/components/boarding_space_card.dart
+++ lib/components/boarding_space_card.dart
@@ -8,88 +8,92 @@ class BoardingSpaceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.only(right: 20, bottom: 40),
-      elevation: 1,
-      child: Container(
-        width: 300,
-        decoration: BoxDecoration(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 300,
-              height: MediaQuery.of(context).size.height / 6 * 2,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(10),
-                  topRight: Radius.circular(10),
-                ),
-                image: DecorationImage(
-                    image: AssetImage(boardingSpace.imageUrl),
-                    fit: BoxFit.cover),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    boardingSpace.name,
-                    style: Theme.of(context).textTheme.titleLarge,
+    return Wrap(
+      children: [
+        Card(
+          margin: const EdgeInsets.only(right: 20),
+          elevation: 1,
+          child: SizedBox(
+            width: 300,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 300,
+                  height: MediaQuery.of(context).size.height / 6 * 2,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                    image: DecorationImage(
+                        image: AssetImage(boardingSpace.imageUrl),
+                        fit: BoxFit.cover),
                   ),
-                  const SizedBox(height: 20),
-                  Row(
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 18.0, vertical: 8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Hourly rates:",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelLarge!
-                                  .copyWith(
-                                      color: Colors.white.withOpacity(0.5)),
-                            ),
-                            const SizedBox(height: 5),
-                            Text(
-                                '${boardingSpace.hourlyRates.toStringAsFixed(2)}/hour',
-                                style: numberSmallText),
-                          ],
-                        ),
+                      Text(
+                        boardingSpace.name,
+                        style: Theme.of(context).textTheme.titleLarge,
                       ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Daily rates:",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelLarge!
-                                  .copyWith(
-                                      color: Colors.white.withOpacity(0.5)),
+                      const SizedBox(height: 20),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Hourly rates:",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelLarge!
+                                      .copyWith(
+                                          color: Colors.white.withOpacity(0.5)),
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                    '${boardingSpace.hourlyRates.toStringAsFixed(2)}/hour',
+                                    style: numberSmallText),
+                              ],
                             ),
-                            const SizedBox(height: 5),
-                            Text(
-                                '${boardingSpace.dailyRates.toStringAsFixed(2)}/day',
-                                style: numberSmallText),
-                          ],
-                        ),
-                      )
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Daily rates:",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelLarge!
+                                      .copyWith(
+                                          color: Colors.white.withOpacity(0.5)),
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                    '${boardingSpace.dailyRates.toStringAsFixed(2)}/day',
+                                    style: numberSmallText),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 20),
                     ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
